c9cf62521e134805114b467ccfd6534f095cb5e3
Tomaka17 <pierre.krieger1708@gmail.com>
2014-09-06 21:58:10 +0200

    The library name is now always "main"

--- apk-builder/src/main.rs
+++ apk-builder/src/main.rs
@@ -44,8 +44,6 @@ fn build_directory(library: &Path, sdk_dir: &Path) -> TempDir {
     use std::io::fs;
     use std::os;
 
-    let library = os::make_absolute(library);
-
     let build_directory = TempDir::new("android-rs-glue-rust-to-apk")
         .expect("Could not create temporary build directory");
 
@@ -70,13 +68,16 @@ fn build_directory(library: &Path, sdk_dir: &Path) -> TempDir {
             .join("native_activity");
         fs::mkdir_recursive(&src_path, std::io::UserRWX).unwrap();
         File::create(&src_path.join("MyNativeActivity.java")).unwrap()
-            .write_str(build_java_class(library.filestem_str().unwrap().slice_from(3)).as_slice())
+            .write_str(build_java_class().as_slice())
             .unwrap();
     }
 
-    let libs_path = build_directory.path().join("libs").join("armeabi");
-    fs::mkdir_recursive(&libs_path, std::io::UserRWX).unwrap();
-    fs::copy(&library, &libs_path.join(library.filename_str().unwrap())).unwrap();
+    {
+        let libs_path = build_directory.path().join("libs").join("armeabi");
+
+        fs::mkdir_recursive(&libs_path, std::io::UserRWX).unwrap();
+        fs::copy(library, &libs_path.join("libmain.so")).unwrap();
+    }
 
     build_directory
 }
@@ -127,7 +128,7 @@ fn build_project_properties() -> String {
     format!(r"target=android-18")
 }
 
-fn build_java_class(libname: &str) -> String {
+fn build_java_class() -> String {
     format!(r#"
 package com.example.native_activity;
 
@@ -136,7 +137,7 @@ import android.util.Log;
 
 public class MyNativeActivity extends NativeActivity {{
   static {{
-    System.loadLibrary("{}");  
+    System.loadLibrary("main");  
   }}
 
   private static String TAG = "MyNativeActivity";
@@ -145,5 +146,5 @@ public class MyNativeActivity extends NativeActivity {{
     super();
     Log.v(TAG, "Creating MyNativeActivity");
   }}
-}}"#, libname)
+}}"#)
 }
