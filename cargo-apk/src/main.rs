f1cb62b39d1a889dafd250729a60effc43a6594a
tomaka <pierre.krieger1708@gmail.com>
2017-04-23 16:29:54 +0200

    Depend on Cargo and use it to localise current manifest

--- cargo-apk/src/main.rs
+++ cargo-apk/src/main.rs
@@ -1,12 +1,13 @@
+extern crate cargo;
 extern crate rustc_serialize;
 extern crate term;
 extern crate toml;
 
 use std::env;
-use std::path::Path;
-use std::path::PathBuf;
-use std::process::exit;
-use std::process::Command;
+
+use cargo::core::Workspace;
+use cargo::util::Config as CargoConfig;
+use cargo::util::important_paths::find_root_manifest_for_wd;
 
 mod build;
 mod config;
@@ -14,39 +15,25 @@ mod install;
 mod termcmd;
 
 fn main() {
+    let cargo_config = CargoConfig::default().unwrap();
+    // TODO: call cargo_config.config(...)
+    let root_manifest = find_root_manifest_for_wd(None /* TODO */, cargo_config.cwd()).unwrap();
+
+    let workspace = Workspace::new(&root_manifest, &cargo_config).unwrap();
+    let current_package = workspace.current().unwrap();
+
     let command = env::args().skip(2).next();
 
-    let current_manifest = current_manifest_path();
-
     // Fetching the configuration for the build.
-    let mut config = config::load(&current_manifest);
+    let mut config = config::load(current_package.manifest_path());
     config.release = env::args().any(|s| &s[..] == "--release");
     if let Some(target_arg_index) = env::args().position(|s| &s[..] == "--bin") {
         config.target = env::args().skip(target_arg_index + 1).next();
     }
 
     if command.as_ref().map(|s| &s[..]) == Some("install") {
-        install::install(&current_manifest, &config);
+        install::install(current_package.manifest_path(), &config);
     } else {
-        build::build(&current_manifest, &config);
+        build::build(current_package.manifest_path(), &config);
     }
-}
-
-/// Returns the path of the `Cargo.toml` that we want to build.
-fn current_manifest_path() -> PathBuf {
-    let output = Command::new("cargo").arg("locate-project").output().unwrap();
-
-    if !output.status.success() {
-        if let Some(code) = output.status.code() {
-            exit(code);
-        } else {
-            exit(-1);
-        }
-    }
-
-    #[derive(RustcDecodable)]
-    struct Data { root: String }
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    let decoded: Data = rustc_serialize::json::decode(&stdout).unwrap();
-    Path::new(&decoded.root).to_owned()
 }
