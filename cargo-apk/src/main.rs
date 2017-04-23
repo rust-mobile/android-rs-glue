6902780a7caf6d4052c4a13fd949ea013a6912a4
tomaka <pierre.krieger1708@gmail.com>
2017-04-23 17:15:37 +0200

    Use Cargo/docopt for the arguments

--- cargo-apk/src/main.rs
+++ cargo-apk/src/main.rs
@@ -16,24 +16,91 @@ mod termcmd;
 
 fn main() {
     let cargo_config = CargoConfig::default().unwrap();
-    // TODO: call cargo_config.config(...)
-    let root_manifest = find_root_manifest_for_wd(None /* TODO */, cargo_config.cwd()).unwrap();
 
-    let workspace = Workspace::new(&root_manifest, &cargo_config).unwrap();
-    let current_package = workspace.current().unwrap();
-
-    let command = env::args().skip(2).next();
-
-    // Fetching the configuration for the build.
-    let mut config = config::load(current_package.manifest_path());
-    config.release = env::args().any(|s| &s[..] == "--release");
-    if let Some(target_arg_index) = env::args().position(|s| &s[..] == "--bin") {
-        config.target = env::args().skip(target_arg_index + 1).next();
-    }
-
-    if command.as_ref().map(|s| &s[..]) == Some("install") {
-        install::install(current_package.manifest_path(), &config);
-    } else {
-        build::build(current_package.manifest_path(), &config);
+    let err = cargo::call_main_without_stdin(execute, &cargo_config, BUILD_USAGE,
+                                             &env::args().collect::<Vec<_>>(), false);
+    
+    match err {
+        Ok(_) => (),
+        Err(err) => cargo::handle_cli_error(err, &mut *cargo_config.shell())    // TODO: use exit_with_error, but it's not yet published on crates.io
     }
 }
+
+pub fn execute(options: Options, cargo_config: &CargoConfig) -> cargo::CliResult {
+    cargo_config.configure(options.flag_verbose,
+                           options.flag_quiet,
+                           &options.flag_color,
+                           options.flag_frozen,
+                           options.flag_locked)?;
+
+    let root_manifest = find_root_manifest_for_wd(options.flag_manifest_path, cargo_config.cwd())?;
+
+    let workspace = Workspace::new(&root_manifest, &cargo_config)?;
+    let current_package = workspace.current()?;
+
+    let mut config = config::load(current_package.manifest_path());
+    config.release = options.flag_release;
+    if !options.flag_bin.is_empty() {
+        config.target = Some(options.flag_bin[0].clone());
+    }
+
+    /*if command.as_ref().map(|s| &s[..]) == Some("install") {
+        install::install(current_package.manifest_path(), &config);
+    } else {*/
+        build::build(current_package.manifest_path(), &config);
+    //}
+
+    Ok(())
+}
+
+#[derive(RustcDecodable)]
+pub struct Options {
+    flag_package: Vec<String>,
+    flag_jobs: Option<u32>,
+    flag_features: Vec<String>,
+    flag_all_features: bool,
+    flag_no_default_features: bool,
+    flag_manifest_path: Option<String>,
+    flag_verbose: u32,
+    flag_quiet: Option<bool>,
+    flag_color: Option<String>,
+    flag_message_format: cargo::ops::MessageFormat,
+    flag_release: bool,
+    flag_lib: bool,
+    flag_bin: Vec<String>,
+    flag_example: Vec<String>,
+    flag_test: Vec<String>,
+    flag_bench: Vec<String>,
+    flag_locked: bool,
+    flag_frozen: bool,
+    flag_all: bool,
+}
+
+const BUILD_USAGE: &'static str = r#"
+Usage:
+    cargo apk [options]
+
+Options:
+    -h, --help                   Print this message
+    -p SPEC, --package SPEC ...  Package to build
+    --all                        Build all packages in the workspace
+    -j N, --jobs N               Number of parallel jobs, defaults to # of CPUs
+    --lib                        Build only this package's library
+    --bin NAME                   Build only the specified binary
+    --example NAME               Build only the specified example
+    --test NAME                  Build only the specified test target
+    --bench NAME                 Build only the specified benchmark target
+    --release                    Build artifacts in release mode, with optimizations
+    --features FEATURES          Space-separated list of features to also build
+    --all-features               Build all available features
+    --no-default-features        Do not build the `default` feature
+    --manifest-path PATH         Path to the manifest to compile
+    -v, --verbose ...            Use verbose output (-vv very verbose/build.rs output)
+    -q, --quiet                  No output printed to stdout
+    --color WHEN                 Coloring: auto, always, never
+    --message-format FMT         Error format: human, json [default: human]
+    --frozen                     Require Cargo.lock and cache are up to date
+    --locked                     Require Cargo.lock is up to date
+
+Does the same as `cargo build`.
+"#;
